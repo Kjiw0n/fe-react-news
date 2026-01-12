6395702a0968e7c7b92785e139769fb93c98a10d
Kjiw0n <rwd0904@naver.com>
2026-01-12 15:23:15 +0900

    style: add color, typography design system

--- fe-react-news/src/App.tsx
+++ fe-react-news/src/App.tsx
@@ -1,5 +1,10 @@
 function App() {
-  return <>fe-react-news</>;
+  return (
+    <>
+      fe-react-news
+      <h1 className="display-bold16 text-weak underline">Hello world!</h1>
+    </>
+  );
 }
 
 export default App;
