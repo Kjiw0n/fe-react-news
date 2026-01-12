91f56a8db5d8cd10722122f2b50c83da60c28c66
Mingyu Seong <minngyuseong@gmail.com>
2026-01-12 16:07:10 +0900

    feat: set up shadcn invironment - Button, Card, Tab 컴포넌트 받아놨습니다.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: true, // 또는 '0.0.0.0'
+    port: 5173,
+  },
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
