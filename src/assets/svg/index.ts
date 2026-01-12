bdb39076bb5678b16391ae1f598123c78d9eaa07
1jiwoo27 <chloey1227@gmail.com>
2026-01-12 16:50:00 +0900

    feat: svgr 세팅

---
+++ src/assets/svg/index.ts
@@ -0,0 +1,15 @@
+import IconArrowLeft from '@/assets/svg/arrow-left.svg';
+import IconArrowRight from '@/assets/svg/arrow-right.svg';
+import IconGridView from '@/assets/svg/grid-view.svg';
+import IconListView from '@/assets/svg/list-view.svg';
+import IconNewsPaper from '@/assets/svg/newspaper.svg';
+
+const Icon = {
+    IconArrowLeft,
+    IconArrowRight,
+    IconGridView,
+    IconListView,
+    IconNewsPaper,
+}
+
+export default Icon;
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,7 +10,15 @@ export default defineConfig({
     host: true, // 또는 '0.0.0.0'
     port: 5173,
   },
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    svgr({
+      svgrOptions: {
+        icon: true,
+      },
+      include: '**/*.svg',
+    })],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
