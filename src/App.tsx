09ef849f0029b4dc042f28134be538500f4ad2b7
Kjiw0n <rwd0904@naver.com>
2026-01-12 17:13:34 +0900

    style: root layout 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { Header } from './components/Header';
+import Header from './components/Header';
 import NewsContentView from './components/news-stand/NewsContentView';
 import NewsRolling from './components/rolling/NewsRolling';
 
 function App() {
   return (
     <>
-      <div>
+      <div className="mx-auto flex h-screen w-232.5 flex-col justify-center">
         <Header />
         <NewsRolling />
         <NewsContentView />
