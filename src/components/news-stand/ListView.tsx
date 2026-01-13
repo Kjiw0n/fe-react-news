5678e21f31ed10bfeadffb108e6d1faa934294df
Kjiw0n <rwd0904@naver.com>
2026-01-13 18:47:24 +0900

    fix: '내가 구독한 언론사' 탭에서  구독한 언론사 없을 시 에러핸들링

--- src/components/news-stand/ListView.tsx
+++ src/components/news-stand/ListView.tsx
@@ -86,56 +86,58 @@ const ListView = ({
         ))}
       </div>
 
-      <div className="border-border-default -mt-px flex flex-col gap-4 border p-6">
-        <div className="flex flex-row items-center justify-start gap-4">
-          <img
-            className="h-5 w-13"
-            src={groupedData[selectedTab][pageIdx].logo}
-            alt="news logo"
-          />
-          <span className="display-medium12 text-default">
-            {groupedData[selectedTab][pageIdx].time}
-          </span>
-          <SubscribeButton
-            pressName={groupedData[selectedTab][pageIdx].press}
-            onClick={switchToSubscribedTab}
-          />
-        </div>
-
-        <div className="flex flex-row gap-8">
-          <a
-            className="flex flex-col gap-4"
-            href={groupedData[selectedTab][pageIdx].mainLink}
-          >
+      {groupedData[selectedTab]?.[pageIdx] && (
+        <div className="border-border-default -mt-px flex flex-col gap-4 border p-6">
+          <div className="flex flex-row items-center justify-start gap-4">
             <img
-              className="h-50 w-[320px] object-cover"
-              src={groupedData[selectedTab][pageIdx].mainImg}
-              alt="news-preview"
+              className="h-5 w-13"
+              src={groupedData[selectedTab][pageIdx].logo}
+              alt="news logo"
             />
-            <span className="available-medium16 text-strong h-11 w-[320px] overflow-y-hidden wrap-break-word whitespace-normal">
-              {groupedData[selectedTab][pageIdx].mainTitle}
+            <span className="display-medium12 text-default">
+              {groupedData[selectedTab][pageIdx].time}
             </span>
-          </a>
-          <div className="flex flex-col gap-4">
-            <ul className="available-medium16 text-bold flex flex-col gap-4">
-              {groupedData[selectedTab][pageIdx].relatedArticles.map(
-                (article) => (
-                  <li
-                    key={article.link}
-                    className="max-w-132.5 cursor-pointer truncate hover:underline"
-                  >
-                    {article.title}
-                  </li>
-                ),
-              )}
-            </ul>
-            <span className="display-medium14 text-weak">
-              {groupedData[selectedTab][pageIdx].press} 언론사에서 직접 편집한
-              뉴스입니다.
-            </span>
+            <SubscribeButton
+              pressName={groupedData[selectedTab][pageIdx].press}
+              onClick={switchToSubscribedTab}
+            />
+          </div>
+
+          <div className="flex flex-row gap-8">
+            <a
+              className="flex flex-col gap-4"
+              href={groupedData[selectedTab][pageIdx].mainLink}
+            >
+              <img
+                className="h-50 w-[320px] object-cover"
+                src={groupedData[selectedTab][pageIdx].mainImg}
+                alt="news-preview"
+              />
+              <span className="available-medium16 text-strong h-11 w-[320px] overflow-y-hidden wrap-break-word whitespace-normal">
+                {groupedData[selectedTab][pageIdx].mainTitle}
+              </span>
+            </a>
+            <div className="flex flex-col gap-4">
+              <ul className="available-medium16 text-bold flex flex-col gap-4">
+                {groupedData[selectedTab][pageIdx].relatedArticles.map(
+                  (article) => (
+                    <li
+                      key={article.link}
+                      className="max-w-132.5 cursor-pointer truncate hover:underline"
+                    >
+                      {article.title}
+                    </li>
+                  ),
+                )}
+              </ul>
+              <span className="display-medium14 text-weak">
+                {groupedData[selectedTab][pageIdx].press} 언론사에서 직접 편집한
+                뉴스입니다.
+              </span>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
