c8b23421234dbf906bb4c0dc3771c9a69ec854aa
1jiwoo27 <chloey1227@gmail.com>
2026-01-13 15:51:06 +0900

    feat: rollingNews 왼쪽 오른쪽 interval과 delay 속성 추가

--- src/components/rolling/RollingSection.tsx
+++ src/components/rolling/RollingSection.tsx
@@ -3,8 +3,8 @@ import NewsRolling from "./NewsRolling";
 const RollingSection = () => {
   return (
     <div className="mb-8 flex w-full gap-2.5">
-      <NewsRolling track="left" />
-      <NewsRolling track="right" interval={5000} />
+      <NewsRolling track="left" interval={5000}/>
+      <NewsRolling track="right" interval={5000} delay={1000} />
     </div>
   );
 };
