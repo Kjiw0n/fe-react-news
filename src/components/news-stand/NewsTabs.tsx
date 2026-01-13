6c970e1cbfe3ec39c08aa0396b4ca95823a2889c
Mingyu Seong <minngyuseong@gmail.com>
2026-01-13 15:10:30 +0900

    refactor: NewsTab 컴포넌트 구현 - 기존 NewsStand에 있는 탭을 컴포넌트로 분화

--- src/components/news-stand/NewsTabs.tsx
+++ src/components/news-stand/NewsTabs.tsx
@@ -1,5 +1,25 @@
+import { cn } from '@/lib/utils';
+import { TabsList, TabsTrigger } from '../ui/tabs';
+
+import { TAB_VALUES } from '@/constants/tabs';
 const NewsTabs = () => {
-  return <div>NewsTabs</div>;
+  return (
+    <TabsList>
+      <TabsTrigger value={TAB_VALUES.ALL}>전체 언론사</TabsTrigger>
+      <TabsTrigger value={TAB_VALUES.SUBSCRIBED}>
+        <span>내가 구독한 언론사</span>
+        <span
+          className={cn(
+            'bg-surface-brand-alt text-white-weak',
+            'display-medium12 flex h-5 w-5 items-center justify-center rounded-full p-0.75',
+            'group-pressData-[state=active]/trigger:bg-surface-brand-default group-pressData-[state=active]/trigger:text-white-default',
+          )}
+        >
+          9
+        </span>
+      </TabsTrigger>
+    </TabsList>
+  );
 };
 
 export default NewsTabs;
---
+++ src/constants/tabs.ts
@@ -0,0 +1,4 @@
+export const TAB_VALUES = {
+  ALL: 'all',
+  SUBSCRIBED: 'subscribed',
+};
