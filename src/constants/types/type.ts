9f973ecade22bc1e44a28bd7bdf57655874b4881
Mingyu Seong <minngyuseong@gmail.com>
2026-01-13 11:48:17 +0900

    feat: pressData을 기반으로 타입 선언 - Category, RelatedArticle, PressData 선언

---
+++ src/constants/types/type.ts
@@ -0,0 +1,29 @@
+// Category Type
+export type Category =
+  | '종합/경제'
+  | '방송/통신'
+  | 'IT'
+  | '영자지'
+  | '스포츠/연예'
+  | '매거진/전문지'
+  | '지역';
+
+// Related Article Type
+export interface RelatedArticle {
+  title: string;
+  link: string;
+}
+
+// Press Data Type
+export interface PressData {
+  category: Category;
+  currentPage: string;
+  totalPage: string;
+  logo: string;
+  press: string;
+  time: string;
+  mainTitle: string;
+  mainLink: string;
+  mainImg: string;
+  relatedArticles: RelatedArticle[];
+}
