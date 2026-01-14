b1e32a8161d1f2420a41c851775f79d21b12dbd1
Kjiw0n <rwd0904@naver.com>
2026-01-14 16:40:21 +0900

    chore: msw(가짜 api서버) install & setting

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,11 @@ import { createRoot } from 'react-dom/client';
 import './styles/index.css';
 import App from './App.tsx';
 
+const { worker } = await import('@/mocks/browser');
+await worker.start({
+  onUnhandledRequest: 'bypass', // 정의되지 않은 API는 통과시킴
+});
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
