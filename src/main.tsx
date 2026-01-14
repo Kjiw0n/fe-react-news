26972ee1064f77f4c052c92c51022055e76c62e0
Mingyu Seong <minngyuseong@gmail.com>
2026-01-14 18:10:17 +0900

    chore: tanstack query 환경설정

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,22 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './styles/index.css';
 import App from './App.tsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 const { worker } = await import('@/mocks/browser');
 await worker.start({
   onUnhandledRequest: 'bypass', // 정의되지 않은 API는 통과시킴
 });
 
+// Create a client
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </StrictMode>,
 );
