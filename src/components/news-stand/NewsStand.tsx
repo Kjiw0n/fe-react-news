b161f6e26a282c44d8edcf16269060f52a719b70
Mingyu Seong <minngyuseong@gmail.com>
2026-01-15 14:32:37 +0900

    feat: 구독개수 Fallback 구현 - useSuspenseQueury로 불러올는 동안 '내가 구독한 언론사(N)'에 보여줄 Fallback을 0개(disabled된 탭) 형태로 구현

--- src/components/news-stand/NewsStand.tsx
+++ src/components/news-stand/NewsStand.tsx
@@ -1,11 +1,10 @@
 import { Tabs, TabsContent } from '../ui/tabs';
 import { TAB_VALUES, type TabValue } from '@/constants/type';
-import { Suspense, useState } from 'react';
+import { useState } from 'react';
 import GridView from './grid-view/GridView';
 import ListView from './list-view/ListView';
 import MediaSourceTab from './tabs/MediaSourceTab';
 import ViewLayoutSwitcher from './tabs/ViewLayoutSwitcher';
-import { Skeleton } from '../ui/skeleton';
 
 const NewsStand = () => {
   const [activeTab, setActiveTab] = useState<TabValue>(TAB_VALUES.ALL);
@@ -18,16 +17,7 @@ const NewsStand = () => {
     <div className="flex flex-col gap-6">
       <Tabs defaultValue={TAB_VALUES.GRID}>
         <div className="flex justify-between">
-          <Suspense
-            fallback={
-              <div className="flex h-fit gap-6">
-                <Skeleton className="h-4 w-18.25" />
-                <Skeleton className="h-4 w-29.75" />
-              </div>
-            }
-          >
-            <MediaSourceTab activeTab={activeTab} onValueChange={switchTab} />
-          </Suspense>
+          <MediaSourceTab activeTab={activeTab} onValueChange={switchTab} />
 
           <ViewLayoutSwitcher />
         </div>
--- src/components/news-stand/tabs/MediaSourceTab.tsx
+++ src/components/news-stand/tabs/MediaSourceTab.tsx
@@ -4,6 +4,7 @@ import { cn } from '@/lib/utils';
 import { useSuspenseQuery } from '@tanstack/react-query';
 import { getSubscribedPresses } from '@/apis/subscription';
 import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { Suspense } from 'react';
 
 interface MediaSourceTabProps {
   activeTab: string;
@@ -11,6 +12,27 @@ interface MediaSourceTabProps {
 }
 
 const MediaSourceTab = ({ activeTab, onValueChange }: MediaSourceTabProps) => {
+  return (
+    <Tabs value={activeTab} onValueChange={onValueChange}>
+      <TabsList className="gap-6">
+        <TabsTrigger className="cursor-pointer" value={TAB_VALUES.ALL}>
+          전체 언론사
+        </TabsTrigger>
+        <Suspense fallback={<SubscribedPressesTabItemFallback />}>
+          <SubscribedPressesTabItem onValueChange={onValueChange} />
+        </Suspense>
+      </TabsList>
+    </Tabs>
+  );
+};
+
+interface SubscribedPressesTabItemProps {
+  onValueChange: (tab: string) => void;
+}
+
+const SubscribedPressesTabItem = ({
+  onValueChange,
+}: SubscribedPressesTabItemProps) => {
   const { data: subscribedPresses = [] } = useSuspenseQuery({
     queryKey: ['subscribedPresses'],
     queryFn: async () => {
@@ -22,31 +44,37 @@ const MediaSourceTab = ({ activeTab, onValueChange }: MediaSourceTabProps) => {
     onValueChange(TAB_VALUES.ALL);
   }
   return (
-    <Tabs value={activeTab} onValueChange={onValueChange}>
-      <TabsList className="gap-6">
-        <TabsTrigger className="cursor-pointer" value={TAB_VALUES.ALL}>
-          전체 언론사
-        </TabsTrigger>
-        <TabsTrigger
-          className="cursor-pointer"
-          value={TAB_VALUES.SUBSCRIBED}
-          disabled={subscribedPresses.length === 0}
-        >
-          <span>내가 구독한 언론사</span>
-          <span
-            className={cn(
-              // 기본 상태 (비활성화 시 색상)
-              'bg-surface-brand-alt text-white-weak',
-              'display-medium12 flex h-5 w-5 items-center justify-center rounded-md p-0.75',
-              // 부모(TabsTrigger)가 active일 때의 색상
-              'group-data-[state=active]/trigger:bg-surface-brand-default group-data-[state=active]/trigger:text-white-default',
-            )}
-          >
-            {subscribedPresses.length}
-          </span>
-        </TabsTrigger>
-      </TabsList>
-    </Tabs>
+    <TabsTrigger
+      className="cursor-pointer"
+      value={TAB_VALUES.SUBSCRIBED}
+      disabled={subscribedPresses.length === 0}
+    >
+      <span>내가 구독한 언론사</span>
+      <span
+        className={cn(
+          // 기본 상태 (비활성화 시 색상)
+          'bg-surface-brand-alt text-white-weak',
+          'display-medium12 flex h-5 w-5 items-center justify-center rounded-md p-0.75',
+          // 부모(TabsTrigger)가 active일 때의 색상
+          'group-data-[state=active]/trigger:bg-surface-brand-default group-data-[state=active]/trigger:text-white-default',
+        )}
+      >
+        {subscribedPresses.length}
+      </span>
+    </TabsTrigger>
+  );
+};
+
+const SubscribedPressesTabItemFallback = () => {
+  return (
+    <TabsTrigger
+      className="cursor-pointer"
+      value={TAB_VALUES.SUBSCRIBED}
+      disabled
+    >
+      <span>내가 구독한 언론사</span>
+      <div className="bg-surface-brand-alt flex h-5 w-5 rounded-md p-0.75" />
+    </TabsTrigger>
   );
 };
 
