0fe10f6c491bd7cf918130429ca3f1a4cd9752a0
Kjiw0n <rwd0904@naver.com>
2026-01-16 11:48:13 +0900

    style: TabsContent min-height 설정(데이터 없을 때 높이가 0px이 되는 ux 문제로 인한 스타일추가)

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -76,7 +76,7 @@ function TabsContent({
   return (
     <TabsPrimitive.Content
       data-slot="tabs-content"
-      className={cn('flex-1 outline-none', className)}
+      className={cn('min-h-100 flex-1 outline-none', className)}
       {...props}
     />
   );
